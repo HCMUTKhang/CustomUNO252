65d04c270e04152f822e1bbbe63f12cbd723ce46
HCMUTKhang <khang.nguyen2352479@hcmut.edu.vn>
2026-05-02 17:24:51 +0700

    change test file, update readme for network layer

---
+++ test_network.py
@@ -0,0 +1,10 @@
+import time
+from client.controller.network_client import NetworkClient
+
+client = NetworkClient(host='127.0.0.1', port=5000)
+print("Connected:", client.connect())
+time.sleep(10)  # Keep connection alive
+
+"""
+python -c "from client.controller.network_client import NetworkClient; c=NetworkClient(host='127.0.0.1', port=5000); print('connected', c.connect())"
+"""
